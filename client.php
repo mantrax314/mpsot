7868602839e78942479ab0b23c07dfc0f0d34040
mantrax314 <mantrax3.14@gmail.com>
2015-08-10 12:22:07 -0430

    change service name

--- client.php
+++ client.php
@@ -2,7 +2,8 @@
 try{
   $urlservice = 'http://prueba/mpsot/app/createrequest.wsdl';
   $soapclient = new SoapClient($urlservice);
-  $soapclient->create('TestClient','12/01/2015 14:05:00','TestComments' );
+  $date = new DateTime();
+  $soapclient->create('TestClient',$date->format('m/d/Y H:i:s'),'TestComments' );
 } catch(SoapFault $e){
  var_dump($e);
 }
